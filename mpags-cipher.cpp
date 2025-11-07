3007b28990c4f0bf75fb94335e25db9b4946acc6
emmaclaraso <emmcla575@gmail.com>
2025-11-07 12:12:58 +0000

    Exercises session 4

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,15 +1,83 @@
 #include <cctype>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <cstdlib>
+#include <boost/program_options.hpp>
 
-int main()
-{
+namespace po = boost::program_options;
+
+
+int main(int argc, char* argv[]){
     // Initialise variables
     char c{'x'};
     std::string out_text;
+    std::string input_file;
+
+    // Define command-line options
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help,h", "produce help message")
+        ("i", po::value<std::string>(&input_file), "Name of input file");
+
+    // Parse the command line
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    // Show help
+    if (vm.count("help")) {
+        std::cout << desc << "\n";
+        return 0;
+    }
+
+    // Use the input file
+    if (vm.count("i")) {
+        std::cout << "Input file: " << input_file << "\n";
+    } else {
+        std::cout << "No input file provided. Use --help for options.\n";
+    }
+
+    return 0;
+
+
+
+    /*
+    const std::vector<std::string> cmdLineArgs { argv, argv+argc };
+
+        
+        for(int i=0; i < cmdLineArgs.size(); i++){
+
+            if ((cmdLineArgs[i] == "--help") || (cmdLineArgs[i] == "-h")) {
+                std::cout << "Help message \n" << std::endl;
+            }
+
+            if (cmdLineArgs[i] == "--version") {
+                std::cout << "Version \n" << std::endl;
+            }
+
+            if (cmdLineArgs[i] == "-o") {
+                std::string out_file{cmdLineArgs[i+1]};
+                std::cout << out_file << std::endl;
+                i++;
+            }
+            
+            if (cmdLineArgs[i] == "-i") {
+                std::string in_file{cmdLineArgs[i+1]};
+                std::cout << in_file << std::endl;
+                i++;
+            }
+
+            //std::cout << cmdLineArgs[i] << std::endl;
+        }
+            */
+
 
     // loop over each character from user input
     while (std::cin >> c) {
+
+        {
+        
         // Uppercase alphabetic characters
         if (std::isalpha(c)) {
             out_text += std::toupper(c);
@@ -55,4 +123,5 @@ int main()
 
     // Print out the transliterated text
     std::cout << out_text << std::endl;
+}
 }
